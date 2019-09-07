8ccc294d8f4490356214ad9a41dcfe2664e890ca
AQ <anna.quinlan123@gmail.com>
2019-09-06 20:53:47 -0700

    Add function to get IOB timeline

--- dose_store.py
+++ dose_store.py
@@ -12,7 +12,8 @@ Github URL: https://github.com/tidepool-org/LoopKit/blob/
 from datetime import timedelta
 
 from dose_math import filter_date_range_for_doses
-from insulin_math import (annotated, trim, glucose_effects, reconciled)
+from insulin_math import (annotated, trim, glucose_effects, reconciled,
+                          insulin_on_board)
 from loop_math import filter_date_range, sort_dose_lists
 
 
@@ -50,6 +51,7 @@ def get_glucose_effects(
     insulin_model -- list in format [DIA (in hours)] if Walsh model, or
                      [DIA (minutes), peak (minutes)] if exponential model
 
+    delay -- time to delay the insulin absorption; defaults to 10 minutes
     end_date -- date to stop calculating glucose effects
 
     Output:
@@ -134,3 +136,103 @@ def get_glucose_effects(
          )
 
     return (filtered_starts, filtered_effect_values)
+
+
+def get_insulin_on_board_values(
+        types, starts, ends, values,
+        start_date,
+        basal_starts, basal_rates, basal_minutes,
+        insulin_model,
+        end_date=None,
+        basal_dosing_end=None,
+        delay=10
+    ):
+    """ Get a timeline of insulin on board (IOB) at a particular time, given
+    a list of doses and a time interval
+
+    Arguments:
+    types -- list of types of dose (basal, bolus, etc)
+    starts -- start dates of the doses (datetime obj)
+    ends -- end dates of the doses (datetime obj)
+    values -- actual basal rates of doses in U/hr (if a basal)
+             or the value of the boluses if in U
+
+    start_date -- date to start calculating glucose effects
+
+    basal_starts -- list of times the basal rates start at
+    basal_rates -- list of basal rates(U/hr)
+    basal_minutes -- list of basal lengths (in mins)
+
+    insulin_model -- list in format [DIA (in hours)] if Walsh model, or
+                     [DIA (minutes), peak (minutes)] if exponential model
+
+    end_date -- date to stop calculating glucose effects
+    delay -- time to delay the insulin absorption; defaults to 10 minutes
+
+    Output:
+    Glucose effects in the format (effect_date, effect_value)
+    """
+    # to properly know glucose effects at start_date,
+    # we need to go back another DIA hours
+    if len(insulin_model) == 1:  # if using Walsh model
+        dose_start = (start_date
+                      - timedelta(
+                          hours=insulin_model[0]
+                          )
+                      )
+    else:
+        dose_start = (start_date
+                      - timedelta(
+                          minutes=insulin_model[0]
+                          )
+                      )
+
+    filtered_doses = filter_date_range_for_doses(
+        types, starts, ends, values,
+        dose_start,
+        end_date
+        )
+
+    # reconcile the doses to get a cleaner data set
+    # (add resumes for suspends and trim any overlapping temp basals)
+    reconciled_doses = reconciled(
+        *filtered_doses
+        )
+    # sort the lists because they could be slightly out of order due to
+    # basals and suspends
+    sorted_reconciled_doses = sort_dose_lists(*reconciled_doses)[0:4]
+
+    # annotate the doses with scheduled basal rate
+    (a_types,
+     a_starts,
+     a_ends,
+     a_values,
+     a_scheduled_rates
+     ) = annotated(
+         *sorted_reconciled_doses,
+         basal_starts, basal_rates, basal_minutes,
+         convert_to_units_hr=False
+     )
+
+    # trim the doses to start of interval
+    for i in range(0, len(a_types)):
+        result = trim(
+            a_types[i], a_starts[i], a_ends[i], a_values[i],
+            a_scheduled_rates[i],
+            start_interval=dose_start,
+            end_interval=basal_dosing_end
+            )
+
+        a_starts[i] = result[1]
+        a_ends[i] = result[2]
+
+    # get the IOB using the prepared dose data
+    iob_timeline = insulin_on_board(
+        a_types, a_starts, a_ends, a_values, a_scheduled_rates,
+        insulin_model,
+        delay=delay,
+        start=start_date,
+        end=end_date
+        )
+
+    return iob_timeline
